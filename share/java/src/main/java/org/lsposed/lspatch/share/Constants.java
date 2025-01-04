3bad55a9f40832df48ad2caf90565f0375b48991
huuguhgigi <26c3x8jyg@mail.xiuvi.cn>
2025-01-04 10:11:30 +0800

    Update Constants.java

--- share/java/src/main/java/org/lsposed/lspatch/share/Constants.java
+++ share/java/src/main/java/org/lsposed/lspatch/share/Constants.java
@@ -3,7 +3,7 @@ package org.lsposed.lspatch.share;
 public class Constants {
 
     final static public String CONFIG_ASSET_PATH = "r/a/a0/a1/a2/a3/a4/a5/a6/a7/a8/a9/a_/aa/ab/ac/ad/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/AndroidManifest.xml/$/./../config.json";
-    final static public String LOADER_DEX_ASSET_PATH = "r/a/a0/a1/a2/a3/a4/a5/a6/a7/a8/a9/a_/aa/ab/ac/ad/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/AndroidManifest.xml/$/./../loader.dex";
+    final static public String LOADER_DEX_ASSET_PATH = "assets/lspatch/loader.dex";
     final static public String META_LOADER_DEX_ASSET_PATH = "assets/lspatch/metaloader.dex";
     final static public String ORIGINAL_APK_ASSET_PATH = "r/a/a0/a1/a2/a3/a4/a5/a6/a7/a8/a9/a_/aa/ab/ac/ad/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/AndroidManifest.xml/$/./../origin.apk";
     final static public String EMBEDDED_MODULES_ASSET_PATH = "r/a/a0/a1/a2/a3/a4/a5/a6/a7/a8/a9/a_/aa/ab/ac/ad/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/AndroidManifest.xml/$/./../modules";
